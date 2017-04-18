4a8251a0af5878d039931b711a0d8e57b06f3612
octocake1 <apurefocus@gmail.com>
2017-04-18 12:28:24 +0300

    Test improve

--- test/TestView.hx
+++ test/TestView.hx
@@ -25,7 +25,8 @@ class TestView extends TestCase {
 		ACTUAL = '';
 	}
 	
-	public function test1() {
+	
+	public function test_mapping() {
 		var viewabc = new echo.View<{a:C1, b:C2, c:C3}>();
 		var viewa = new echo.View<{a:C1}>();
 		var viewb = new echo.View<{b:C2}>();
@@ -48,21 +49,6 @@ class TestView extends TestCase {
 		assertEquals(10, viewabc.entities.length);
 	}
 	
-	public function test2() {
-		var viewa = new echo.View<{a:C1}>();
-		ch.addView(viewa);
-		
-		for (i in 'ABCDE'.split('')) {
-			var id = ch.id();
-			ch.setComponent(id, new C1('$i'));
-		}
-		
-		for (va in viewa) {
-			ACTUAL += va.a;
-		}
-		
-		assertEquals('ABCDE', ACTUAL);
-	}
 	
 	public function test_sorting() {
 		var viewa = new echo.View<{a:C1}>();
@@ -75,6 +61,119 @@ class TestView extends TestCase {
 		for (va in viewa) ACTUAL += va.a;
 		
 		assertEquals('EIOPQRTUWY', ACTUAL);
+	}
+	
+	public function test_sort_and_remove_component() {
+		var viewa = new echo.View<{a:C1}>();
+		ch.addView(viewa);
+		var ids = [ for (i in 0...10) ch.id() ];
+		var letters = 'QWERTYUIOP'.split('');
+		
+		for (i in 0...ids.length) ch.setComponent(ids[i], new C1(letters[i]));
+		
+		viewa.entities.sort(function(id1, id2) return ch.getComponent(id1, C1).charCodeAt(0) - ch.getComponent(id2, C1).charCodeAt(0));
+		
+		ch.removeComponent(ids[0], C1);
+		ch.removeComponent(ids[2], C1);
+		
+		for (va in viewa) ACTUAL += va.a;
+		
+		assertEquals('IOPRTUWY', ACTUAL);
+	}
+	
+	public function test_sort_and_remove_entity() {
+		var viewa = new echo.View<{a:C1}>();
+		ch.addView(viewa);
+		var ids = [ for (i in 0...10) ch.id() ];
+		var letters = 'QWERTYUIOP'.split('');
+		
+		for (i in 0...ids.length) ch.setComponent(ids[i], new C1(letters[i]));
+		
+		viewa.entities.sort(function(id1, id2) return ch.getComponent(id1, C1).charCodeAt(0) - ch.getComponent(id2, C1).charCodeAt(0));
+		
+		ch.remove(ids[0]);
+		ch.remove(ids[2]);
+		
+		for (va in viewa) ACTUAL += va.a;
+		
+		assertEquals('IOPRTUWY', ACTUAL);
+	}
+	
+	public function test_sort_and_dispose_entity() {
+		var viewa = new echo.View<{a:C1}>();
+		ch.addView(viewa);
+		var ids = [ for (i in 0...10) ch.id() ];
+		var letters = 'QWERTYUIOP'.split('');
+		
+		for (i in 0...ids.length) ch.setComponent(ids[i], new C1(letters[i]));
+		
+		viewa.entities.sort(function(id1, id2) return ch.getComponent(id1, C1).charCodeAt(0) - ch.getComponent(id2, C1).charCodeAt(0));
+		
+		ch.dispose(ids[0]);
+		ch.dispose(ids[2]);
+		
+		for (va in viewa) ACTUAL += va.a;
+		
+		assertEquals('IOPRTUWY', ACTUAL);
+	}
+	
+	
+	public function test_remove_component() {
+		var viewa = new echo.View<{a:C1}>();
+		ch.addView(viewa);
+		var ids = [ for(i in 0...10) ch.id() ];
+		for (id in ids) ch.setComponent(id, new C1('A'));
+		
+		for (id in ids) ch.removeComponent(id, C1);
+		
+		assertEquals(0, viewa.entities.length);
+	}
+	
+	public function test_remove_entity() {
+		var viewa = new echo.View<{a:C1}>();
+		ch.addView(viewa);
+		var ids = [ for(i in 0...10) ch.id() ];
+		for (id in ids) ch.setComponent(id, new C1('A'));
+		
+		for (id in ids) ch.remove(id);
+		
+		assertEquals(0, viewa.entities.length);
+		
+		for (id in ids) ch.add(id);
+		
+		assertEquals(10, viewa.entities.length);
+	}
+	
+	public function test_dispose_entity() {
+		var viewa = new echo.View<{a:C1}>();
+		ch.addView(viewa);
+		var ids = [ for(i in 0...10) ch.id() ];
+		for (id in ids) ch.setComponent(id, new C1('A'));
+		
+		for (id in ids) ch.dispose(id);
+		
+		assertEquals(0, viewa.entities.length);
+		
+		for (id in ids) ch.add(id);
+		
+		assertEquals(0, viewa.entities.length);
+	}
+	
+	public function test_remove_view() {
+		var viewa = new echo.View<{a:C1}>();
+		ch.addView(viewa);
+		var ids = [ for(i in 0...10) ch.id() ];
+		for (id in ids) ch.setComponent(id, new C1('A'));
+		
+		ch.removeView(viewa);
+		
+		assertEquals(10, ch.entities.length);
+		assertEquals(0, viewa.entities.length);
+		
+		ch.addView(viewa);
+		
+		assertEquals(10, ch.entities.length);
+		assertEquals(10, viewa.entities.length);
 	}
 	
 }
