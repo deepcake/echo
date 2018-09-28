1a1ac7998daadd46b08c9feb0afb77674c2e3f0d
deepcake <apurefocus@gmail.com>
2018-09-28 16:46:19 +0300

    comment cc trace

--- src/echo/macro/ComponentMacro.hx
+++ src/echo/macro/ComponentMacro.hx
@@ -35,7 +35,7 @@ class ComponentMacro {
 
         } catch (err:String) {
 
-            trace('not found $componentContainerClsName');
+            //trace('not found $componentContainerClsName');
 
             ++componentIndex;
 
