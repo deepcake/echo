9874ca2d9b3e154a876208dd7a6c86861cae7c82
deepcake <apurefocus@gmail.com>
2019-08-25 18:54:17 +0300

    add simple build test

---
+++ test/BuildTest.hx
@@ -0,0 +1,75 @@
+using buddy.Should;
+
+import echos.View;
+import echos.Workflow;
+import echos.Entity;
+
+class BuildTest extends buddy.BuddySuite {
+    public function new() {
+        describe("Building", {
+
+            beforeEach(echos.Workflow.dispose());
+
+            describe("When define a View with different ways", {
+                beforeEach(echos.Workflow.addSystem(new DefineViewSystem()));
+                it("should define equaled views", {
+                    DefineViewSystem.reversed.should.be(DefineViewSystem.original);
+                    DefineViewSystem.param.should.be(DefineViewSystem.original);
+                    DefineViewSystem.paramTypedef.should.be(DefineViewSystem.original);
+                    DefineViewSystem.viewTypedef.should.be(DefineViewSystem.original);
+                    DefineViewSystem.short.should.be(DefineViewSystem.original);
+                });
+                it("should add only one view to the flow", {
+                    echos.Workflow.views.length.should.be(1);
+                });
+            });
+
+        });
+    }
+}
+
+abstract CompA(Int) {
+    public function new() {
+        this = 1;
+    }
+}
+
+abstract CompB(Int) {
+    public function new() {
+        this = 2;
+    }
+}
+
+typedef ParamTypedef = { a:CompA, b:CompB };
+
+typedef ViewTypedef = View<{ a:CompA, b:CompB }>;
+
+class DefineViewSystem extends echos.System {
+
+    public static var original:View<CompA->CompB->Void>;
+
+    public static var reversed:View<CompB->CompA->Void>;
+
+    public static var param:View<{ a:CompA, b:CompB }>;
+
+    public static var paramTypedef:View<ParamTypedef>;
+
+    public static var viewTypedef:ViewTypedef;
+
+    public static var short:View<CompA->CompB>;
+
+    @u function ab(a:CompA, b:CompB) { }
+
+    @u function ba(b:CompB, a:CompA) { }
+
+    @u function cd(c:CompB, d:CompA) { }
+
+    @u function eab(f:Float, a:CompA, b:CompB) { }
+
+    @u function fab(e:Entity, a:CompA, b:CompB) { }
+
+    @u function feab(f:Float, e:Entity, a:CompA, b:CompB) { }
+
+    @u function fiab(f:Float, i:Int, a:CompA, b:CompB) { }
+
+}
--- test/Run.hx
+++ test/Run.hx
@@ -1,6 +1,7 @@
 package;
 
 class Run implements buddy.Buddy<[ 
+    new BuildTest(),
     new SignalTest(),
     new ComponentTypeTest(), 
     new EntityTest(), 
