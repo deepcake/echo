912fbf491f15036a47354a49627b4a4ec293f418
deepcake <apurefocus@gmail.com>
2020-05-02 16:54:29 +0300

    bit more system test checks

--- test/SystemTest.hx
+++ test/SystemTest.hx
@@ -69,12 +69,15 @@ class SystemTest extends buddy.BuddySuite {
             });
 
             describe("When create System (meta @added/@removed)", {
-                var s1 = new SystemAddRemMeta();
                 var entities:Array<Entity>;
+                var s1 = new SystemAddRemMeta();
 
                 describe("Then create Entities", {
                     beforeEach({
                         entities = [ for (i in 0...2) new Entity().add(new CompA(), new CompB(), new CompC()) ];
+                    });
+                    it("should have correct result", {
+                        BuildResult.value.should.be('');
                     });
 
                     describe("Then add System to the flow", {
@@ -87,6 +90,15 @@ class SystemTest extends buddy.BuddySuite {
                             beforeEach(Workflow.removeSystem(s1));
                             it("should have correct result", {
                                 BuildResult.value.should.be('+A+A>A>A+Ae+Ae<A-A-Ae<A-A-Ae');
+                            });
+
+                            describe("Then destroy Entities", {
+                                beforeEach({
+                                    for (e in entities) e.destroy();
+                                });
+                                it("should have correct result", {
+                                    BuildResult.value.should.be('+A+A>A>A+Ae+Ae<A-A-Ae<A-A-Ae');
+                                });
                             });
                         });
                     });
@@ -238,15 +250,15 @@ abstract CompC(String) {
 
 class SystemUpdateMeta extends echoes.System {
     @u function empty0() BuildResult.value += '[';
-    @u function _f(f:Float) BuildResult.value += '$f';
+    @u function _f____(f:Float) BuildResult.value += '$f';
     @u function empty1() BuildResult.value += '_';
-    @u function _a(a:CompA) BuildResult.value += '$a';
+    @u function _a____(a:CompA) BuildResult.value += '$a';
     @u function empty2() BuildResult.value += '_';
-    @u function _fa(f:Float, a:CompA) BuildResult.value += '${f}${a}';
+    @u function _fa___(f:Float, a:CompA) BuildResult.value += '${f}${a}';
     @u function empty3() BuildResult.value += '_';
-    @u function _fe(f:Float, e:Entity) BuildResult.value += '${f}e';
+    @u function _fe___(f:Float, e:Entity) BuildResult.value += '${f}e';
     @u function empty4() BuildResult.value += '_';
-    @u function _fea(f:Float, e:Entity, a:CompA) BuildResult.value += '${f}e${a}';
+    @u function _fea__(f:Float, e:Entity, a:CompA) BuildResult.value += '${f}e${a}';
     @u function empty5() BuildResult.value += ']';
     override function onactivate() {
         BuildResult.value += '^';
