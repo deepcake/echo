1194de6465ca17e8f240418b076b36416be557d0
deepcake <apurefocus@gmail.com>
2020-05-02 16:35:39 +0300

    updated entity and system tests

--- test/SystemTest.hx
+++ test/SystemTest.hx
@@ -6,46 +6,60 @@ import echoes.Entity;
 
 class SystemTest extends buddy.BuddySuite {
     public function new() {
-        describe("System", {
+        describe("Test System", {
 
             beforeEach({
                 Workflow.reset();
                 BuildResult.value = '';
             });
 
-            describe("Using @update metas", {
-                var updSys = new UpdateMetaGeneration();
+            describe("When create System (meta @update)", {
+                var s1 = new SystemUpdateMeta();
 
-                describe("When add System", {
-                    beforeEach(Workflow.addSystem(updSys));
+                describe("Then update", {
+                    beforeEach(Workflow.update(0));
                     it("should have correct result", {
-                        BuildResult.value.should.be('^');
+                        BuildResult.value.should.be('');
                     });
 
-                    describe("When add Entities", {
-                        beforeEach({
-                            for (i in 0...2) new Entity().add(new CompA(), new CompB(), new CompC());
-                        });
+                    describe("Then add system to the flow", {
+                        beforeEach(Workflow.addSystem(s1));
                         it("should have correct result", {
                             BuildResult.value.should.be('^');
                         });
 
-                        describe("When update", {
+                        describe("Then update", {
                             beforeEach(Workflow.update(0));
                             it("should have correct result", {
-                                BuildResult.value.should.be('^_0_AA_0A0A_0e0e_0eA0eA_');
+                                BuildResult.value.should.be('^[0____]');
                             });
 
-                            describe("When remove System", {
-                                beforeEach(Workflow.removeSystem(updSys));
-                                it("should have correct result", {
-                                    BuildResult.value.should.be('^_0_AA_0A0A_0e0e_0eA0eA_$');
+                            describe("Then add Entities", {
+                                beforeEach({
+                                    for (i in 0...2) new Entity().add(new CompA(), new CompB(), new CompC());
                                 });
-
-                                describe("When update", {
+                                it("should have correct result", {
+                                    BuildResult.value.should.be('^[0____]');
+                                });
+            
+                                describe("Then update", {
                                     beforeEach(Workflow.update(0));
                                     it("should have correct result", {
-                                        BuildResult.value.should.be('^_0_AA_0A0A_0e0e_0eA0eA_$');
+                                        BuildResult.value.should.be('^[0____][0_AA_0A0A_0e0e_0eA0eA]');
+                                    });
+            
+                                    describe("Then remove System from the flow", {
+                                        beforeEach(Workflow.removeSystem(s1));
+                                        it("should have correct result", {
+                                            BuildResult.value.should.be('^[0____][0_AA_0A0A_0e0e_0eA0eA]$');
+                                        });
+            
+                                        describe("Then update", {
+                                            beforeEach(Workflow.update(0));
+                                            it("should have correct result", {
+                                                BuildResult.value.should.be('^[0____][0_AA_0A0A_0e0e_0eA0eA]$');
+                                            });
+                                        });
                                     });
                                 });
                             });
@@ -54,23 +68,23 @@ class SystemTest extends buddy.BuddySuite {
                 });
             });
 
-            describe("Using @added and @removed metas", {
-                var sys1 = new AddedRemovedMetaGeneration();
+            describe("When create System (meta @added/@removed)", {
+                var s1 = new SystemAddRemMeta();
                 var entities:Array<Entity>;
 
-                describe("When add System with already created Entities", {
+                describe("Then create Entities", {
                     beforeEach({
                         entities = [ for (i in 0...2) new Entity().add(new CompA(), new CompB(), new CompC()) ];
                     });
 
-                    describe("When add System to the flow", {
-                        beforeEach(Workflow.addSystem(sys1));
+                    describe("Then add System to the flow", {
+                        beforeEach(Workflow.addSystem(s1));
                         it("should have correct result", {
                             BuildResult.value.should.be('+A+A>A>A+Ae+Ae');
                         });
 
-                        describe("When remove System from the flow", {
-                            beforeEach(Workflow.removeSystem(sys1));
+                        describe("Then remove System from the flow", {
+                            beforeEach(Workflow.removeSystem(s1));
                             it("should have correct result", {
                                 BuildResult.value.should.be('+A+A>A>A+Ae+Ae<A-A-Ae<A-A-Ae');
                             });
@@ -78,14 +92,24 @@ class SystemTest extends buddy.BuddySuite {
                     });
                 });
 
-                describe("When add a first System", {
-                    beforeEach(Workflow.addSystem(sys1));
+                describe("Then add System to the flow", {
+                    beforeEach(Workflow.addSystem(s1));
                     it("should correctly add listeners", {
-                        StandaloneA.a.onAdded.size().should.be(3);
-                        StandaloneA.a.onRemoved.size().should.be(3);
+                        ViewCompA.inst().isActive().should.be(true);
+                        ViewCompA.inst().onAdded.size().should.be(3);
+                        ViewCompA.inst().onRemoved.size().should.be(3);
                     });
 
-                    describe("When create Entities", {
+                    describe("Then remove System from the flow", {
+                        beforeEach(Workflow.removeSystem(s1));
+                        it("should correctly remove listeners", {
+                            ViewCompA.inst().isActive().should.be(false);
+                            ViewCompA.inst().onAdded.size().should.be(0);
+                            ViewCompA.inst().onRemoved.size().should.be(0);
+                        });
+                    });
+
+                    describe("Then create Entities", {
                         beforeEach({
                             entities = [ for (i in 0...2) new Entity().add(new CompA(), new CompB(), new CompC()) ];
                         });
@@ -93,7 +117,7 @@ class SystemTest extends buddy.BuddySuite {
                             BuildResult.value.should.be('+A>A+Ae+A>A+Ae');
                         });
 
-                        describe("When destroy Entities", {
+                        describe("Then destroy Entities", {
                             beforeEach({
                                 for (e in entities) e.destroy();
                             });
@@ -103,88 +127,34 @@ class SystemTest extends buddy.BuddySuite {
                         });
                     });
 
-                    describe("When remove a first System", {
-                        beforeEach(Workflow.removeSystem(sys1));
-                        it("should correctly remove listeners", {
-                            StandaloneA.a.onAdded.size().should.be(0);
-                            StandaloneA.a.onRemoved.size().should.be(0);
-                        });
-                    });
+                    describe("Then add a second System with equal View to the flow", {
+                        var s2 = new SystemAddRemMeta2();
 
-                    describe("When add a second System with equal View", {
-                        var sys2 = new AddedRemovedAdditionalMetaGeneration();
-
-                        beforeEach(Workflow.addSystem(sys2));
+                        beforeEach(Workflow.addSystem(s2));
                         it("should correctly add listeners", {
-                            StandaloneA.a.onAdded.size().should.be(4);
-                            StandaloneA.a.onRemoved.size().should.be(4);
+                            ViewCompA.inst().isActive().should.be(true);
+                            ViewCompA.inst().onAdded.size().should.be(4);
+                            ViewCompA.inst().onRemoved.size().should.be(4);
                         });
 
-                        describe("When create Entities", {
-                            beforeEach({
-                                entities = [ for (i in 0...2) new Entity().add(new CompA(), new CompB(), new CompC()) ];
-                            });
-                            it("should have correct result", {
-                                BuildResult.value.should.be('+A>A+Ae!+A>A+Ae!');
-                            });
-
-                            describe("When destroy Entities", {
-                                beforeEach({
-                                    for (e in entities) e.destroy();
-                                });
-                                it("should have correct result", {
-                                    BuildResult.value.should.be('+A>A+Ae!+A>A+Ae!<A-A-Ae#<A-A-Ae#');
-                                });
-                            });
-
-                            describe("When remove a first System", {
-                                beforeEach(Workflow.removeSystem(sys1));
-                                it("should have correct result", {
-                                    BuildResult.value.should.be('+A>A+Ae!+A>A+Ae!');
-                                });
-
-                                describe("When destroy Entities", {
-                                    beforeEach({
-                                        for (e in entities) e.destroy();
-                                    });
-                                    it("should have correct result", {
-                                        BuildResult.value.should.be('+A>A+Ae!+A>A+Ae!##');
-                                    });
-                                });
-                            });
-
-                            describe("When remove a second System with equal View", {
-                                beforeEach(Workflow.removeSystem(sys2));
-                                it("should have correct result", {
-                                    BuildResult.value.should.be('+A>A+Ae!+A>A+Ae!');
-                                });
-
-                                describe("When destroy Entities", {
-                                    beforeEach({
-                                        for (e in entities) e.destroy();
-                                    });
-                                    it("should have correct result", {
-                                        BuildResult.value.should.be('+A>A+Ae!+A>A+Ae!<A-A-Ae<A-A-Ae');
-                                    });
-                                });
-                            });
-
-                            describe("When reset", {
-                                beforeEach(Workflow.reset());
-                                it("should have correct result", {
-                                    BuildResult.value.should.be('+A>A+Ae!+A>A+Ae!<A-A-Ae#<A-A-Ae#');
-                                });
-                            });
-                        });
-
-                        describe("When remove a second System with equal View", {
-                            beforeEach(Workflow.removeSystem(sys2));
+                        describe("Then remove a second System with equal View from the flow", {
+                            beforeEach(Workflow.removeSystem(s2));
                             it("should correctly remove listeners", {
-                                StandaloneA.a.onAdded.size().should.be(3);
-                                StandaloneA.a.onRemoved.size().should.be(3);
+                                ViewCompA.inst().isActive().should.be(true);
+                                ViewCompA.inst().onAdded.size().should.be(3);
+                                ViewCompA.inst().onRemoved.size().should.be(3);
                             });
 
-                            describe("When create Entities", {
+                            describe("Then remove a first System from the flow", {
+                                beforeEach(Workflow.removeSystem(s1));
+                                it("should correctly remove listeners", {
+                                    ViewCompA.inst().isActive().should.be(false);
+                                    ViewCompA.inst().onAdded.size().should.be(0);
+                                    ViewCompA.inst().onRemoved.size().should.be(0);
+                                });
+                            });
+
+                            describe("Then create Entities", {
                                 beforeEach({
                                     entities = [ for (i in 0...2) new Entity().add(new CompA(), new CompB(), new CompC()) ];
                                 });
@@ -192,7 +162,7 @@ class SystemTest extends buddy.BuddySuite {
                                     BuildResult.value.should.be('+A>A+Ae+A>A+Ae');
                                 });
 
-                                describe("When destroy Entities", {
+                                describe("Then destroy Entities", {
                                     beforeEach({
                                         for (e in entities) e.destroy();
                                     });
@@ -203,14 +173,15 @@ class SystemTest extends buddy.BuddySuite {
                             });
                         });
 
-                        describe("When remove a first System", {
-                            beforeEach(Workflow.removeSystem(sys1));
+                        describe("Then remove a first System from the flow", {
+                            beforeEach(Workflow.removeSystem(s1));
                             it("should correctly remove listeners", {
-                                StandaloneA.a.onAdded.size().should.be(1);
-                                StandaloneA.a.onRemoved.size().should.be(1);
+                                ViewCompA.inst().isActive().should.be(true);
+                                ViewCompA.inst().onAdded.size().should.be(1);
+                                ViewCompA.inst().onRemoved.size().should.be(1);
                             });
 
-                            describe("When create Entities", {
+                            describe("Then create Entities", {
                                 beforeEach({
                                     entities = [ for (i in 0...2) new Entity().add(new CompA(), new CompB(), new CompC()) ];
                                 });
@@ -218,13 +189,31 @@ class SystemTest extends buddy.BuddySuite {
                                     BuildResult.value.should.be('!!');
                                 });
 
-                                describe("When destroy Entities", {
+                                describe("Then destroy Entities", {
                                     beforeEach({
                                         for (e in entities) e.destroy();
                                     });
                                     it("should have correct result", {
                                         BuildResult.value.should.be('!!##');
                                     });
+                                });
+                            });
+                        });
+
+                        describe("Then create Entities", {
+                            beforeEach({
+                                entities = [ for (i in 0...2) new Entity().add(new CompA(), new CompB(), new CompC()) ];
+                            });
+                            it("should have correct result", {
+                                BuildResult.value.should.be('+A>A+Ae!+A>A+Ae!');
+                            });
+
+                            describe("Then destroy Entities", {
+                                beforeEach({
+                                    for (e in entities) e.destroy();
+                                });
+                                it("should have correct result", {
+                                    BuildResult.value.should.be('+A>A+Ae!+A>A+Ae!<A-A-Ae#<A-A-Ae#');
                                 });
                             });
                         });
@@ -247,8 +236,8 @@ abstract CompC(String) {
     public function new() this = 'C';
 }
 
-class UpdateMetaGeneration extends echoes.System {
-    @u function empty0() BuildResult.value += '_';
+class SystemUpdateMeta extends echoes.System {
+    @u function empty0() BuildResult.value += '[';
     @u function _f(f:Float) BuildResult.value += '$f';
     @u function empty1() BuildResult.value += '_';
     @u function _a(a:CompA) BuildResult.value += '$a';
@@ -258,7 +247,7 @@ class UpdateMetaGeneration extends echoes.System {
     @u function _fe(f:Float, e:Entity) BuildResult.value += '${f}e';
     @u function empty4() BuildResult.value += '_';
     @u function _fea(f:Float, e:Entity, a:CompA) BuildResult.value += '${f}e${a}';
-    @u function empty5() BuildResult.value += '_';
+    @u function empty5() BuildResult.value += ']';
     override function onactivate() {
         BuildResult.value += '^';
     }
@@ -267,7 +256,7 @@ class UpdateMetaGeneration extends echoes.System {
     }
 }
 
-class AddedRemovedMetaGeneration extends echoes.System {
+class SystemAddRemMeta extends echoes.System {
     @a function ad_a1(a:CompA) BuildResult.value += '+${a}';
     @a function ad_a2(a:CompA) BuildResult.value += '>${a}';
     @r function rm_a2(a:CompA) BuildResult.value += '<${a}';
@@ -276,14 +265,12 @@ class AddedRemovedMetaGeneration extends echoes.System {
     @r function rm_ae(a:CompA, e:Entity) BuildResult.value += '-${a}e';
 }
 
-class AddedRemovedAdditionalMetaGeneration extends echoes.System {
+class SystemAddRemMeta2 extends echoes.System {
     @a function ad_a(a:CompA) BuildResult.value += '!';
     @r function rm_a(a:CompA) BuildResult.value += '#';
 }
 
-class StandaloneA extends echoes.System {
-    public static var a:View<CompA>;
-}
+typedef ViewCompA = View<CompA>;
 
 class BuildResult {
     public static var value = '';
