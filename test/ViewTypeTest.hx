34d9ac929a658f893d2c4d2723867d9669b23f4f
deepcake <apurefocus@gmail.com>
2020-06-14 21:14:24 +0300

    func component view test

--- test/ViewTypeTest.hx
+++ test/ViewTypeTest.hx
@@ -13,25 +13,25 @@ class ViewTypeTest extends buddy.BuddySuite {
                 Workflow.reset();
             });
 
-            describe("When define a View with Different Type Params", {
-                var sys:ViewTypeSystem;
+            describe("When define a View with Type Params", {
+                var tvs:TypeViewSystem;
 
-                beforeEach(sys = new ViewTypeSystem());
+                beforeEach(tvs = new TypeViewSystem());
 
                 it("should be equals", {
-                    sys.rest.should.be(StandaloneVAVBSystem.ab);
-                    sys.restReversed.should.be(StandaloneVAVBSystem.ab);
+                    tvs.rest.should.be(StandaloneTypeViewSystem.ab);
+                    tvs.restReversed.should.be(StandaloneTypeViewSystem.ab);
                 });
 
                 describe("When add System to the flow", {
-                    beforeEach(Workflow.addSystem(sys));
+                    beforeEach(Workflow.addSystem(tvs));
 
                     it("should have correct count of views", {
                         Workflow.views.length.should.be(1);
                     });
 
                     describe("When add standalone System to the flow", {
-                        beforeEach(Workflow.addSystem(new StandaloneVAVBSystem()));
+                        beforeEach(Workflow.addSystem(new StandaloneTypeViewSystem()));
 
                         it("should have correct count of views", {
                             Workflow.views.length.should.be(1);
@@ -39,7 +39,42 @@ class ViewTypeTest extends buddy.BuddySuite {
                     });
 
                     describe("When remove System from the flow", {
-                        beforeEach(Workflow.removeSystem(sys));
+                        beforeEach(Workflow.removeSystem(tvs));
+
+                        it("should have correct count of views", {
+                            Workflow.views.length.should.be(0);
+                        });
+                    });
+                });
+            });
+
+            describe("When define a View with Func Params", {
+                var fvs:FuncViewSystem;
+
+                beforeEach(fvs = new FuncViewSystem());
+
+                it("should be equals", {
+                    fvs.fv.should.be(StandaloneFuncViewSystem.fv);
+                    fvs.fvReversed.should.be(StandaloneFuncViewSystem.fv);
+                });
+
+                describe("When add System to the flow", {
+                    beforeEach(Workflow.addSystem(fvs));
+
+                    it("should have correct count of views", {
+                        Workflow.views.length.should.be(1);
+                    });
+
+                    describe("When add standalone System to the flow", {
+                        beforeEach(Workflow.addSystem(new StandaloneFuncViewSystem()));
+
+                        it("should have correct count of views", {
+                            Workflow.views.length.should.be(1);
+                        });
+                    });
+
+                    describe("When remove System from the flow", {
+                        beforeEach(Workflow.removeSystem(fvs));
 
                         it("should have correct count of views", {
                             Workflow.views.length.should.be(0);
@@ -65,7 +100,7 @@ abstract VC(String) {
 }
 
 
-class ViewTypeSystem extends echoes.System {
+class TypeViewSystem extends echoes.System {
 
     public var rest:View<VA, VB>;
 
@@ -87,6 +122,24 @@ class ViewTypeSystem extends echoes.System {
 
 }
 
-class StandaloneVAVBSystem extends echoes.System {
+class FuncViewSystem extends echoes.System {
+
+    public var fv:View<VA->VB->Void, VA->VB>;
+
+    public var fvReversed:View<VA->VB, VA->VB->Void>;
+
+    @u function abv_ab(abv:VA->VB->Void, ab:VA->VB) { }
+
+    @u function ab_abv(ab:VA->VB, abv:VA->VB->Void) { }
+
+    @u function dt_e_abv_ab(f:Float, e:Entity, abv:VA->VB->Void, ab:VA->VB) { }
+
+}
+
+class StandaloneTypeViewSystem extends echoes.System {
     public static var ab:View<VA, VB>;
+}
+
+class StandaloneFuncViewSystem extends echoes.System {
+    public static var fv:View<VA->VB->Void, VA->VB>;
 }
